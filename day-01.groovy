313c02b2d23838c60ebcac8e61d0a8836355eafe
Rob Faust <robert.faust@labelzoom.net>
2022-12-05 13:43:50 -0600

    Day 1

---
+++ day-01.groovy
@@ -0,0 +1,20 @@
+def current = 0, max = 0;
+def file = new File('day-01.input.txt');
+file.withReader { reader ->
+    def line = null;
+    // Loop through the file one line at a time, attempting to parse each line into an int
+    // If parsing fails, we'll assume we encountered a blank/empty line, which means we need
+    // to reset our "current" counter. Every time we reset our counter, or once we reach the
+    // end of the file, we'll update our max to be the greater of the previous max or
+    // current counter
+    while ((line = reader.readLine()) != null) {
+        try {
+            current += Integer.parseInt(line);
+        } catch (NumberFormatException ex) {
+            max = Math.max(max, current);
+            current = 0;
+        }
+    }
+    max = Math.max(max, current);
+}
+println(max);
