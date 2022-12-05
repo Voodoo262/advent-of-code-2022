153b541d578a5414e0ae0ceefcd9a3b3126450d0
Rob Faust <robert.faust@labelzoom.net>
2022-12-05 13:56:31 -0600

    Day 1 Part 2

---
+++ day-01-part-02.groovy
@@ -0,0 +1,36 @@
+def current = 0;
+def leaderboard = new PriorityQueue(new Comparator<Integer>() {
+    public int compare(Integer elf1, Integer elf2) {
+        return elf2 - elf1;
+    }
+});
+def file = new File('day-01.input.txt');
+file.withReader { reader ->
+    def line = null;
+    // Loop through the file one line at a time, attempting to parse each line into an int
+    // If parsing fails, we'll assume we encountered a blank/empty line, which means we need
+    // to reset our "current" counter. Every time we reset our counter, we'll add the value
+    // to our queue
+    while ((line = reader.readLine()) != null) {
+        try {
+            current += Integer.parseInt(line);
+        } catch (NumberFormatException ex) {
+            leaderboard.add(current);
+            current = 0;
+        }
+    }
+    if (current > 0) {
+        leaderboard.add(current);
+    }
+}
+
+// Grab the top 3 entries, erroring if we have less than 3
+def total = 0;
+for (def i = 0; i < 3; i++) {
+    def val = leaderboard.poll();
+    if (val == null) {
+        throw new RuntimeException("less than 3 elements in our queue");
+    }
+    total += val;
+}
+println(total);
